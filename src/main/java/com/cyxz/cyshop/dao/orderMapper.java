2cf9765b38e7f128990e8462c586faa5c5bae2a9
sharluokup <gaoyongsongbo@163.com>
2019-09-17 11:01:59 +0800

    订单列表加了servlet

---
+++ src/main/java/com/cyxz/cyshop/dao/orderMapper.java
@@ -0,0 +1,15 @@
+package com.cyxz.cyshop.dao;
+
+import com.cyxz.cyshop.domain.Order;
+
+import java.util.List;
+
+/**
+ * @author 高勇
+ * @version 1.0.0
+ * @date 2019/9/1615:12
+ * @place 西部国际金融中心
+ */
+public interface orderMapper {
+    List<Order> getOrderByMemberId(String memberId);
+}
---
+++ src/main/java/com/cyxz/cyshop/web/servlet/orderDetailServlet.java
@@ -0,0 +1,25 @@
+package com.cyxz.cyshop.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author 高勇
+ * @version 1.0.0
+ * @date 2019/9/179:48
+ * @place 西部国际金融中心
+ */
+public class orderDetailServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
