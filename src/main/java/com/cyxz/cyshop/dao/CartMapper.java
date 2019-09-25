f6134e3d605cb9d32c5065a6a19cce5066b0382b
wutong <602232262@qq.com>
2019-09-25 20:55:22 +0800

    前端商品展示,加入购物车

--- src/main/java/com/cyxz/cyshop/dao/CartMapper.java
+++ src/main/java/com/cyxz/cyshop/dao/CartMapper.java
@@ -1,6 +1,7 @@
 package com.cyxz.cyshop.dao;
 
 import com.cyxz.cyshop.domain.CartDomain;
+import com.cyxz.cyshop.domain.ShoppingCartItem;
 
 import java.util.List;
 
@@ -15,5 +16,6 @@ public interface CartMapper {
     // 通过购物车id删除购物车信息
     Integer deleteCartById(String CartId);
 
+    int addCartItem(ShoppingCartItem shoppingCartItem);
 
 }
